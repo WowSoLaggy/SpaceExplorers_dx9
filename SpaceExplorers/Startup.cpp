f20516c723a328947ff8e9e0058cc868eedde7a9
Anton Egorov <dohxehapo@gmail.com>
2017-01-23 23:45:38 +0300

    Removed all to start from the very begining

--- SpaceExplorers/Startup.cpp
+++ SpaceExplorers/Startup.cpp
@@ -1,9 +1,7 @@
 #include "stdafx.h"
-#include "Engine.h"
 
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-	Engine engine;
-	return engine.Run({ hInstance, hPrevInstance, lpCmdLine, nCmdShow });
+	return 0;
 }
