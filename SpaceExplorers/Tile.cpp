2db1eb3db760d86a7a0f842e7adc43e31d0923d8
Anton Egorov <dohxehapo@gmail.com>
2017-09-04 19:12:02 +0300

    (+) Added a turf to the Tile

--- SpaceExplorers/Tile.cpp
+++ SpaceExplorers/Tile.cpp
@@ -2,17 +2,59 @@
 #include "Tile.h"
 
 
+Tile::Tile()
+  : d_turf(nullptr)
+{
+}
+
 Tile::~Tile()
 {
+  resetTurf();
 }
 
 
 bool Tile::updateSelf(float pDt)
 {
+  if (d_turf)
+  {
+    if (!d_turf->update(pDt))
+      return false;
+  }
+
   return true;
 }
 
 bool Tile::drawSelf(Doh3d::Sprite& pSprite) const
 {
+  if (d_turf)
+  {
+    if (!d_turf->draw(pSprite))
+      return false;
+  }
+
   return true;
+}
+
+
+void Tile::setTurf(GameObject* i_turf)
+{
+  if (i_turf == d_turf)
+    return;
+
+  if (d_turf)
+    resetTurf();
+
+  if (!i_turf)
+    return;
+
+  d_turf = i_turf;
+}
+
+void Tile::resetTurf()
+{
+  if (d_turf)
+  {
+    delete d_turf;
+    d_turf = nullptr;
+  }
 }
--- SpaceExplorers/Tile.h
+++ SpaceExplorers/Tile.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "GameObject.h"
 #include "SceneObject.h"
 
 
@@ -7,9 +8,18 @@ class Tile : public SceneObject
 {
 public:
 
+  Tile();
   virtual ~Tile();
 
+  void setTurf(GameObject* i_turf);
+  void resetTurf();
+
+  const GameObject* getTurf() const { return d_turf; }
+  GameObject* getTurf() { return d_turf; }
+
 private:
+
+  GameObject* d_turf;
 
   virtual bool updateSelf(float pDt) override;
   virtual bool drawSelf(Doh3d::Sprite& pSprite) const override;
