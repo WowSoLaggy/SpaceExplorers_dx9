1cbf8d15ecb2c88b0b77948195c7642cae984277
Anton Egorov <dohxehapo@gmail.com>
2017-09-05 14:23:30 +0300

    (*) Added acceleration to the movement model

--- SpaceExplorers/MovementModel.cpp
+++ SpaceExplorers/MovementModel.cpp
@@ -6,23 +6,55 @@
 
 bool MovementModel::update(float i_dt)
 {
+  const float MAX_ACCEL = Tile::DEFAULT_TILE_SIZE * 20;
   const float MAX_SPEED = Tile::DEFAULT_TILE_SIZE * 3;
+  const float MIN_SPEED = 20;
 
-  Doh3d::Vector2F currentSpeed{ 0, 0 };
+  // Calc current accel
+
+  Doh3d::Vector2F currentAccel{ 0, 0 };
+  Doh3d::Vector2F speed = getSpeed();
 
   if (d_goUp != d_goDown)
   {
-    if (d_goUp) currentSpeed.y = -MAX_SPEED;
-    else currentSpeed.y = MAX_SPEED;
+    if (d_goUp) currentAccel.y = -MAX_ACCEL;
+    else currentAccel.y = MAX_ACCEL;
+  }
+  else
+  {
+    if (speed.y > MIN_SPEED) currentAccel.y = -MAX_ACCEL;
+    else if (speed.y < -MIN_SPEED) currentAccel.y = MAX_ACCEL;
+    else speed.y = 0;
   }
 
   if (d_goLeft != d_goRight)
   {
-    if (d_goLeft) currentSpeed.x = -MAX_SPEED;
-    else currentSpeed.x = MAX_SPEED;
+    if (d_goLeft) currentAccel.x = -MAX_ACCEL;
+    else currentAccel.x = MAX_ACCEL;
+  }
+  else
+  {
+    if (speed.x > MIN_SPEED) currentAccel.x = -MAX_ACCEL;
+    else if (speed.x < -MIN_SPEED) currentAccel.x = MAX_ACCEL;
+    else speed.x = 0;
   }
 
-  setSpeed(currentSpeed);
+  setAccel(currentAccel);
+  setSpeed(speed);
 
-  return Doh3d::IMovable::update(i_dt);
+  if (!Doh3d::IMovable::update(i_dt))
+    return false;
+
+  // Clamp maximum speed
+
+  speed = getSpeed();
+  float speedLength = speed.length();
+  
+  if (speedLength > MAX_SPEED)
+  {
+    speed /= speedLength / MAX_SPEED;
+    setSpeed(speed);
+  }
+
+  return true;
 }
