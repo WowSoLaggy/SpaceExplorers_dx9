033b237dd94cf60b29a879137be1988819088d94
Anton Egorov <dohxehapo@gmail.com>
2017-09-05 19:18:17 +0300

    (+) Added camera binding

--- SpaceExplorers/Camera.cpp
+++ SpaceExplorers/Camera.cpp
@@ -1,2 +1,35 @@
 #include "stdafx.h"
 #include "Camera.h"
+
+#include "GameObject.h"
+
+
+void Camera::bindToObject(const GameObject* i_object)
+{
+  if (d_bindedObject == i_object)
+    return;
+
+  unbindFromObject();
+
+  if (!i_object)
+    return;
+
+  d_bindedObject = i_object;
+}
+
+void Camera::unbindFromObject()
+{
+  if (d_bindedObject)
+    d_bindedObject = nullptr;
+}
+
+
+bool Camera::updateSelf(float i_dt)
+{
+  if (!d_bindedObject)
+    return true;
+
+  setPosition(d_bindedObject->getMovementModel().getPosition() - Doh3d::Screen::getClientCenter());
+
+  return true;
+}
--- SpaceExplorers/Camera.h
+++ SpaceExplorers/Camera.h
@@ -3,6 +3,20 @@
 #include "SceneObject.h"
 
 
+class GameObject;
+
+
 class Camera : public Doh3d::Camera, public SceneObject
 {
+public:
+
+  void bindToObject(const GameObject* i_object);
+  void unbindFromObject();
+
+private:
+
+  const GameObject* d_bindedObject;
+
+  virtual bool updateSelf(float i_dt) override;
+
 };
