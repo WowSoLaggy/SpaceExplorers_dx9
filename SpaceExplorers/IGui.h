88a26b73116e7c5604e8ad4187721d6987a248fa
Anton Egorov <dohxehapo@gmail.com>
2017-09-11 00:02:02 +0300

    (~) Correct class name

--- SpaceExplorers/IGui.h
+++ SpaceExplorers/IGui.h
@@ -7,7 +7,7 @@ namespace View
 class IGui
   : public Doh3d::IPositioned2I
   , public Doh3d::ITextured
-  , public Doh3d::ISized2
+  , public Doh3d::ISized2I
   , public Doh3d::IInputHandler
   , public Doh3d::IUpdatable
   , public Doh3d::IDrawable
