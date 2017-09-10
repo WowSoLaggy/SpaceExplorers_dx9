2587c482020cb3576307caa6795f5fb98766a887
Anton Egorov <dohxehapo@gmail.com>
2017-09-10 23:42:17 +0300

    (~) Removed excessive namespace

--- SpaceExplorers/IGui.cpp
+++ SpaceExplorers/IGui.cpp
@@ -7,7 +7,7 @@ namespace View
 
 bool IGui::containsPoint(Doh3d::Position2I pPoint)
 {
-  return Doh3d::Geometry::RectI(getPosition(), getSize()).containsPoint(pPoint);
+  return Doh3d::RectI(getPosition(), getSize()).containsPoint(pPoint);
 }
 
 } // ns View
